b238e7e632adf2fa27a893749e0bab4f0704ebe3
Kerija E. Baumane <kerija.baumane@gmail.com>
2023-12-09 00:27:59 +0200

    Corrected style and media queries after tests on different screens

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+// Validēt formu - ja nav ievadīta summa, nevar aprēķināt maksājumus
+// Ja nav ievadīts amout, poga nav aktīva, ja ir - poga ir aktīva un viņu var nospiest, lai reset formu
+// reset poga visu reseto uz sākotnējo stāvokli
+// formā tiek automātiski viss aprēķināts, mainot summu un procentus
+// jāaprēķina mēneša un kopējais maksājums un jāizvada $0.00 vietā
